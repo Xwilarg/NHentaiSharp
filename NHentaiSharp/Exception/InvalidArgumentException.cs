646e020446b671e80d3b7686d667121b12d67616
Xwilarg <xwilarg@yahoo.fr>
2019-10-25 00:22:24 +0900

    Fix typo

--- NHentaiSharp/Exception/InvalidArgumentException.cs
+++ NHentaiSharp/Exception/InvalidArgumentException.cs
@@ -2,7 +2,7 @@
 {
     public class InvalidArgumentException : System.Exception
     {
-        public InvalidArgumentException() : base("Your search didn't return any result, that probably mean one of the argument your provided is invalid")
+        public InvalidArgumentException() : base("Your search didn't return any result, that probably mean one of the argument you provided is invalid")
         { }
     }
 }
